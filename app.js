79472f003455cf2f35ff53383134fb0ed388e9e1
gaurav <grvgulia007@gmail.com>
2022-06-18 00:19:02 +0530

    code angela yu

--- app.js
+++ app.js
@@ -12,46 +12,10 @@ app.use(favicon(path.join(__dirname, "public", "images/favicon.ico")));
 app.set("view engine", "ejs");
 
 // making the constants
-
-const day1 = `lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam odio possimus maxime, rem nesciunt
-                voluptas, expedita impedit temporibus omnis, quasi magni qui asperiores voluptatem. Repellendus debitis
-                aliquam quod voluptas doloremque adipisci omnis ipsa officiis odit voluptatem maiores fuga tempora, iure
-                repudiandae ab labore possimus itaque blanditiis laboriosam. Debitis aliquid porro impedit eveniet, quae
-                eos
-                voluptatibus cupiditate exercitationem provident rem quos suscipit, necessitatibus odit. Laudantium ad
-                eum
-                tempore. Delectus necessitatibus alias porro doloribus, molestiae magnam velit, nemo non impedit ad
-                soluta!
-                Placeat cumque nihil, aut aspernatur officia laboriosam! Recusandae beatae doloribus dolore nobis
-                sapiente
-                unde eius magnam culpa nostrum nesciunt ipsa numquam iure fugit doloremque, quos saepe voluptas
-                cupiditate
-                deleniti libero laboriosam deserunt? Veniam at atque quibusdam tempora iure placeat praesentium rerum,
-                asperiores non officia minima blanditiis fuga sint, eligendi nisi harum qui dolorem aliquam autem
-                expedita
-                itaque
-            `;
-
-const day2 = `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam odio possimus maxime, rem nesciunt
-                voluptas, expedita impedit temporibus omnis, quasi magni qui asperiores voluptatem. Repellendus debitis
-                aliquam quod voluptas doloremque adipisci omnis ipsa officiis odit voluptatem maiores fuga tempora, iure
-                repudiandae ab labore possimus itaque blanditiis laboriosam. Debitis aliquid porro impedit eveniet, quae
-                eos
-                voluptatibus cupiditate exercitationem provident rem quos suscipit, necessitatibus odit. Laudantium ad
-                eum
-                tempore. Delectus necessitatibus alias porro doloribus, molestiae magnam velit, nemo non impedit ad
-                soluta!
-                Placeat cumque nihil, aut aspernatur officia laboriosam! Recusandae beatae doloribus dolore nobis
-                sapiente
-                unde eius magnam culpa nostrum nesciunt ipsa numquam iure fugit doloremque, quos saepe voluptas
-                cupiditate
-                deleniti libero laboriosam deserunt? Veniam at atque quibusdam tempora iure placeat praesentium rerum,
-                asperiores non officia minima blanditiis fuga sint, eligendi nisi harum qui dolorem aliquam autem
-                expedita`;
+const day1 = ` "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus cum, p"`;
+let posts = [];
 
 // cancel and publish buttons used in new.ejs view
-
-let yes = "";
 
 // routing starts here
 app.listen("3000", (req, res) => {
@@ -59,7 +23,10 @@ app.listen("3000", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.render("app", { para1: day1, para2: day2 });
+  res.render("app", {
+    para1: day1,
+    posts: posts,
+  });
 });
 
 app.post("/contact", (req, res) => {
@@ -94,7 +61,8 @@ app.post("/", (req, res) => {
   if (req.body.cancel === "cancelled") {
     res.redirect("/");
   } else {
-    let data = req.body;
-    res.render("app", { para1: day1, para2: day2 });
+    const blog = req.body.blog;
+    posts.push(blog);
+    res.redirect("/");
   }
 });
